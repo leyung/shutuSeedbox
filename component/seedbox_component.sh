bc625af50b76434de3b7776b9295bd85f6b6c637
kongbai <444569120@qq.com>
2023-03-03 19:54:18 +0800

    update

--- component/seedbox_component.sh
+++ component/seedbox_component.sh
@@ -103,5 +103,5 @@ function bbrx_install() {
 function boot_script() {
   echo -e "\033[36m ================= 杰佬 boot-script ================= \033[0m"
   . <(wget -qO- https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Miscellaneous/.boot-script.sh)
-  boot_script
+  .boot_script
 }
